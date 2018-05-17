f31be11741efa810cecee8676090f8d70377aee3
Ormadont <evgeniy1demyanov@gmail.com>
2018-05-17 21:25:49 +0400

    add small fix

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import './Business.css';
 
-//How to import a css file in a react component?
-// https://stackoverflow.com/questions/39853646/how-to-import-a-css-file-in-a-react-component
-
-let business = {
+const business = {
   imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
   name: 'MarginOtto Pizzeria',
   address: '1010 Paddington Way',
@@ -14,11 +11,11 @@ let business = {
   category: 'Italian',
   rating: 4.5,
   reviewCount: 90
-}
+};
 
 class Business extends React.Component {
   render() {
-    return(
+    return (
       <div className="Business">
         <div className="image-container">
           <img src={business.imageSrc} alt=''/>
@@ -28,10 +25,10 @@ class Business extends React.Component {
           <div className="Business-address">
             <p>{business.address}</p>
             <p>{business.city}</p>
-            <p>{business.state + ' ' + business.zipCode}</p>
+            <p>{`${business.state} ${business.zipCode}`}</p>
           </div>
           <div className="Business-reviews">
-            <h3>{business.category}</h3>
+            <h3>{business.category.toUpperCase()}</h3>
             <h3 className="rating">{business.rating} stars</h3>
             <p>{business.reviewCount} reviews</p>
           </div>
